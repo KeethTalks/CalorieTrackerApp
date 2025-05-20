6a47446de41eb4c0480284c6f2af6e14019c6f5c
KeethTalks <sankeethan.ratneswaran@gmail.com>
2025-05-19 23:04:00 -0400

    firebase deploy

---
+++ functions/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    "ecmaVersion": 2018,
+  },
+  extends: [
+    "eslint:recommended",
+    "google",
+  ],
+  rules: {
+    "no-restricted-globals": ["error", "name", "length"],
+    "prefer-arrow-callback": "error",
+    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "no-unused-vars": "warn",
+    "no-console": "warn",    
+  },
+  overrides: [
+    {
+      files: ["**/*.spec.*"],
+      env: {
+        mocha: true,
+      },
+      rules: {},
+    },
+  ],
+  globals: {},
+};
---
+++ functions/index.js
@@ -0,0 +1,51 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const { processQuery, generateResponse } = require("../queryProcessor"); // Import RAG functions
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Endpoint to handle RAG queries
+app.post("/rag-query", async (req, res) => {
+    try {
+        const { query, namespace } = req.body;
+        if (!query || !namespace) {
+            return res.status(400).json({ error: "Missing query or namespace" });
+        }
+
+        // Retrieve relevant documents
+        const retrievedDocs = await processQuery(query, namespace);
+        
+        // Generate AI response
+        const aiResponse = await generateResponse(query, retrievedDocs);
+
+        res.json({ response: aiResponse });
+    } catch (error) {
+        console.error("Error processing RAG request:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+// Expose API function
+exports.api = functions.https.onRequest(app);
